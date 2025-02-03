05309cb552e452a14006702eab6d75826b31ac5f
Mohammad Muzammil <162691043+muzammil1244@users.noreply.github.com>
2025-02-03 19:52:55 +0530

    Update index.js

--- index.js
+++ index.js
@@ -40,6 +40,7 @@ const options ={
         servers:[
             {
                 url:"http://localhost:8001"
+                 url:"https://job-portal-vsl5.onrender.com
             }
         ]
     },
